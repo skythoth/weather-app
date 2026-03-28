ac03eaf00c1c262c43d54f06f92464dad78f7db5
skythoth <skythoth@me.com>
2026-03-29 01:40:07 +0900

    🎨 loading spinner

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import './App.css'
 import WeatherBox from './component/WeatherBox'
 import WeatherButton from './component/WeatherButton'
+import { ClipLoader } from "react-spinners";
 
 // 앱이 실행되자마자 현재 위치 기반의 날씨가 보인다
 // 날시 정보에는 도시, 섭씨, 화씨, 날시 정보가 보인다
@@ -16,23 +17,28 @@ const API_KEY = 'd2472977222e45c1eab40e21d94933c9'
 function App() {
   const [weather, setWeather] = useState(null)
   const [currentCity, setCurrentCity] = useState('')
+  const [loading, setLoading] = useState(false)
 
   //위치에 따른 날씨정보 받아오기
   const getWeatherByCurrentLocation = async (lat, lon) => {
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    setLoading(true)
     let response = await fetch(url)
     let data = await response.json()
     setWeather(data)
     setCurrentCity('current')
+    setLoading(false)
   }
 
   //도시별 날짜정보 받아오기
   const getWeatherByCity = async (city) => {
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    setLoading(true)
     let response = await fetch(url)
     let data = await response.json()
     setWeather(data)
     setCurrentCity(city)
+    setLoading(false)
   }
 
   //현재 위치 정보 받아오기
@@ -49,16 +55,35 @@ function App() {
     getCurrentLocation()
   }, [])
 
+  // useEffect(() => {
+  //   if (currentCity === 'current') {
+  //     getWeatherByCurrentLocation()
+  //   } else {
+  //     getWeatherByCity(currentCity)
+  //   }
+  // }, [currentCity])
+
   return (
     <div className="container">
       <h1>Weather App</h1>
 
-      <WeatherBox weather={weather} />
-      <WeatherButton
-        currentCity={currentCity}
-        getCurrentLocation={getCurrentLocation}
-        getWeatherByCity={getWeatherByCity}
-      />
+      {loading ? (
+        <ClipLoader
+          color='#666'
+          loading={loading}
+          size={150}
+          aria-label="Loading Spinner"
+          data-testid="loader" />
+      ) : (
+        <>
+          <WeatherBox weather={weather} />
+          <WeatherButton
+            currentCity={currentCity}
+            getCurrentLocation={getCurrentLocation}
+            getWeatherByCity={getWeatherByCity}
+          />
+        </>
+      )}
     </div>
   )
 }
