94177135dde5e46d1ef9278186cb9011e83c3e65
skythoth <skythoth@me.com>
2026-03-29 01:45:37 +0900

    🎨 try catch 이용, 데이터 로드 에러시 무한 스피너 방지

--- src/App.jsx
+++ src/App.jsx
@@ -22,32 +22,50 @@ function App() {
   //위치에 따른 날씨정보 받아오기
   const getWeatherByCurrentLocation = async (lat, lon) => {
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
-    setLoading(true)
-    let response = await fetch(url)
-    let data = await response.json()
-    setWeather(data)
-    setCurrentCity('current')
-    setLoading(false)
+
+    try {
+      setLoading(true)
+      // fetch & setWeather 로직
+      let response = await fetch(url)
+      let data = await response.json()
+      setWeather(data)
+      setCurrentCity('current')
+    } catch (error) {
+      console.log(error)
+    } finally {
+      // loading은 성공/실패 모두 꺼야 하니까 여기!
+      setLoading(false)
+    }
   }
 
   //도시별 날짜정보 받아오기
   const getWeatherByCity = async (city) => {
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
-    setLoading(true)
-    let response = await fetch(url)
-    let data = await response.json()
-    setWeather(data)
-    setCurrentCity(city)
-    setLoading(false)
+
+    try {
+      setLoading(true)
+      let response = await fetch(url)
+      let data = await response.json()
+      setWeather(data)
+      setCurrentCity(city)
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setLoading(false)
+    }
   }
 
   //현재 위치 정보 받아오기
   const getCurrentLocation = () => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      let lat = position.coords.latitude
-      let lon = position.coords.longitude
-      getWeatherByCurrentLocation(lat, lon)
-    })
+    try {
+      navigator.geolocation.getCurrentPosition((position) => {
+        let lat = position.coords.latitude
+        let lon = position.coords.longitude
+        getWeatherByCurrentLocation(lat, lon)
+      })
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   //앱이 실행되자마자 현재 위치 기반의 날씨가 보인다
